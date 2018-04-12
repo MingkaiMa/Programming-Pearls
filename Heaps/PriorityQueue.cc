d84b7c8db469de1b3bf0fe7b9ad7f85596c7c19c
Mingkai Ma <mamingkai824@gmail.com>
2018-04-12 20:40:39 +1000

    heaps

---
+++ Heaps/PriorityQueue.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+
+class prequeue
+{
+private:
+    int n, maxsize;
+    T *x;
+
+    void swap(int i, int j)
+    {
+        T t = x[i];
+        x[i] = x[j];
+        x[j] = t;
+    }
+
+public:
+    prequeue(int m)
+    {
+        maxsize = m;
+        x = new T[maxsize + 1];
+        n = 0;
+    }
+
+    void insert(T t)
+    {
+        int i, p;
+        x[++n] = t;
+        for(i = n; i > 1 && x[p = i / 2] > x[i]; i = p)
+            swap(p, i);
+    }
+
+    T extractmin()
+    {
+        int i, c;
+        T t = x[1];
+        for(i = 1; (c = 2 * i) <= n; i = c)
+        {
+            if(c + 1 <= n && x[c + 1] < x[c])
+                c++;
+            if(x[i] <= x[c])
+                break;
+            swap(c, i);
+        }
+        return t;
+    }
+    
+}
