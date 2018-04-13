618c745c8da36c91808db3031401b9bd02049ded
Mingkai Ma <mamingkai824@gmail.com>
2018-04-13 20:15:16 +1000

    mapStringCount

---
+++ Strings-Of-Pearls/mapStringCount.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    map<string, int> M;
+    map<string, int>::iterator j;
+    string t;
+
+    while(cin >> t)
+        M[t]++;
+
+    for(j = M.begin(); j != M.end(); j++)
+        cout << j->first << " " << j->second >> "\n";
+}
